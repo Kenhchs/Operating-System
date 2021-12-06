195755b127eaeb083fdd8643db3345a65cdf5b4b
Kenhchs <86562503+Kenhchs@users.noreply.github.com>
2021-12-07 00:32:21 +0800

    Add files via upload

--- myshell.c
+++ myshell.c
@@ -18,6 +18,11 @@ void Process_String(char *str);
 char* substring(char *str ,int start, int end);
 void print_history(int all, int num);
 int muti_zero(char*, int);
+void My_echo(char*);
+void My_clear(char*);
+void My_cd(char*);
+void My_export(char*);
+void My_history(char*);
 
 char hist[2048][1024];
 int hist_num = -1;
@@ -58,8 +63,9 @@ int Input(char* str)
 }
 void Process_String(char *str)
 {
-	char temp[1024];
+	char temp[1024],pass[1024];
 	strcpy(temp, str);
+	strcpy(pass,str);
 	if(hist_num >= 2047)
 	{
 		printf("Excess of history.");
@@ -77,21 +83,7 @@ void Process_String(char *str)
 	char* token = strtok(temp," ");
 	if(strcmp(token,"echo") == 0)
 	{
-		token = strtok(NULL," ");
-		while(token != NULL)
-		{
-			if(token[0] == '$')
-			{
-				char *substr = substring(token ,1 , strlen(token) - 1);
-				printf("%s ",getenv(substr));
-			}
-			else
-			{
-				printf("%s ",token);	
-			}
-			token = strtok(NULL," ");
-		}
-		printf("\n");
+		My_echo(&pass[0]);
 	}
 	else if(strcmp(token,"pwd") == 0)
 	{
@@ -101,125 +93,19 @@ void Process_String(char *str)
 	}
 	else if(strcmp(token,"export") == 0)
 	{
-		char* temp_path = malloc(4096);
-		strcpy(temp_path,"");
-		token = strtok(NULL,"=");
-		char *variable = token;
-		
-		token = strtok(NULL,":");
-		while(token != NULL)
-		{
-			if(strcmp(temp_path,"") == 0);
-			else
-			{
-				strcat(temp_path,":");
-			}
-			if(token[0] == '$')
-			{
-				char *substr = substring(token ,1 , strlen(token) - 1);
-				if(getenv(substr) != NULL)
-				{
-					strcat(temp_path,getenv(substr));
-				}
-			}
-			else
-			{
-				strcat(temp_path,token);
-			}
-			token = strtok(NULL,":");
-		}
-		setenv(variable,temp_path,1);
+		My_export(&pass[0]);
 	}
 	else if(strcmp(token,"cd") == 0)//assume that file's name doesn't contain spaces
 	{
-		int count = 0;
-		token = strtok(NULL," ");
-		while(token != NULL)
-		{
-			count++;
-			if(count >= 2)
-			{
-				printf("cd: too many arguments\n");
-				break;
-			}
-			strcpy(temp,token);
-			token = strtok(NULL," ");
-		}
-		if(count == 1)
-		{
-			if(chdir(temp) < 0)
-			{
-				printf("cd: %s: No such file or directory\n",temp);
-			}
-		}
-		else if(count == 0)
-		{
-			char* username = getenv("USER");
-			strcpy(temp,"/home/");
-			strcat(temp,username);
-			chdir(temp);
-		}
+		My_cd(&pass[0]);
 	}
 	else if(strcmp(token,"clear") == 0)
 	{
-		int count = 0;
-		token = strtok(NULL," ");
-		while(token != NULL)
-		{
-			count++;
-			if(count >= 1)
-			{
-				printf("Could not execute command\n");
-				break;
-			}
-			strcpy(temp,token);
-			token = strtok(NULL," ");
-		}
-		if(count == 0)
-		{
-			clear();
-		}
+		My_clear(&pass[0]);
 	}
 	else if(strcmp(token,"history") == 0)
 	{
-		int count = 0, num;
-		token = strtok(NULL," ");
-		while(token != NULL)
-		{
-			int val = atoi(token);
-			if(val == 0 && muti_zero(token, strlen(token) - 1) == 0)
-			{
-				if(count >= 1)
-				{
-					printf("history: too many arguments\n");
-				}
-				else
-				{
-					printf("history: %s: numeric argument required\n",token);	
-				}
-				return;
-			}
-			else if(val == 0 && muti_zero(token, strlen(token) - 1) == 1);
-			else if(val != 0)
-			{
-				count += 1;
-				if(count >= 2)
-				{
-					printf("history: too many arguments\n");
-					return;
-				}
-				num = val;
-			}
-			token = strtok(NULL," ");
-		}
-		if(count == 0)
-		{
-			print_history(1, num);
-		}
-		if(count == 1)
-		{
-			print_history(0, num);		
-		}
+		My_history(&pass[0]);
 	}
 	else
 	{
@@ -250,7 +136,7 @@ void Process_String(char *str)
 			{
 				printf("Could not execute command\n");
 			}
-			exit(0);
+			return;
 		}
 		else
 		{
@@ -309,4 +195,177 @@ int muti_zero(char *str, int num)
 		}
 	}
 	return 1;
+}
+void My_echo(char *temp)
+{
+	char* token = strtok(temp," ");
+	token = strtok(NULL," ");
+	while(token != NULL)
+	{
+		if(token[0] == '$')
+		{
+			char *substr = substring(token ,1 , strlen(token) - 1);
+			printf("%s ",getenv(substr));
+		}
+		else
+		{
+			printf("%s ",token);	
+		}
+		token = strtok(NULL," ");
+	}
+	printf("\n");
+	return;
+}
+void My_clear(char *temp)
+{
+	char* token = strtok(temp," ");
+	token = strtok(NULL," ");
+	int count = 0;
+	while(token != NULL)
+	{
+		count++;
+		if(count >= 1)
+		{
+			printf("Could not execute command\n");
+			break;
+		}
+		strcpy(temp,token);
+		token = strtok(NULL," ");
+	}
+	if(count == 0)
+	{
+		clear();
+	}
+	return;
+}
+void My_cd(char *temp)
+{
+	char* token = strtok(temp," ");
+	int count = 0;
+	token = strtok(NULL," ");
+	while(token != NULL)
+	{
+		count++;
+		if(count >= 2)
+		{
+			printf("cd: too many arguments\n");
+			break;
+		}
+		strcpy(temp,token);
+		token = strtok(NULL," ");
+	}
+	if(count == 1)
+	{
+		if(chdir(temp) < 0)
+		{
+			printf("cd: %s: No such file or directory\n",temp);
+		}
+	}
+	else if(count == 0)
+	{
+		char* username = getenv("USER");
+		strcpy(temp,"/home/");
+		strcat(temp,username);
+		chdir(temp);
+	}
+	return;
+}
+void My_export(char *temp)
+{
+	char* token = strtok(temp," ");
+	char* temp_path = malloc(4096);
+	strcpy(temp_path,"");
+	token = strtok(NULL,"=");
+	char *variable = token;
+	
+	token = strtok(NULL,":");
+	while(token != NULL)
+	{
+		if(strcmp(temp_path,"") == 0);
+		else
+		{
+			strcat(temp_path,":");
+		}
+		if(token[0] == '$')
+		{
+			char *substr = substring(token ,1 , strlen(token) - 1);
+			if(getenv(substr) != NULL)
+			{
+				strcat(temp_path,getenv(substr));
+			}
+		}
+		else
+		{
+			strcat(temp_path,token);
+		}
+		token = strtok(NULL,":");
+	}
+	if(strcmp(temp_path,"") == 0)
+	{
+		char *exports[60] = {"CLUTTER_IM_MODULE","COLORTERM","DBUS_SESSION_BUS_ADDRESS","DESKTOP_AUTOSTART_ID","DESKTOP_SESSION","DISPLAY","GDMSESSION","GNOME_DESKTOP_SESSION_ID","GNOME_SHELL_SESSION_MODE","GNOME_TERMINAL_SCREEN","GNOME_TERMINAL_SERVICE","GPG_AGENT_INFO","GTK_IM_MODULE","GTK_MODULES","HOME","IM_CONFIG_PHASE","LANG","LC_ADDRESS","LC_IDENTIFICATION","LC_MEASUREMENT","LC_MONETARY","LC_NAME","LC_NUMERIC","LC_PAPER","LC_TELEPHONE","LC_TIME","LESSCLOSE","LESSOPEN","LOGNAME","LS_COLORS","OLDPWD","PATH","PWD","QT4_IM_MODULE","QT_ACCESSIBILITY","QT_IM_MODULE","SESSION_MANAGER","SHELL","SHLVL","SSH_AGENT_PID","SSH_AUTH_SOCK","TERM","TEXTDOMAIN","TEXTDOMAINDIR","USER","USERNAME","VTE_VERSION","WINDOWPATH","XAUTHORITY","XDG_CONFIG_DIRS","XDG_CURRENT_DESKTOP","XDG_DATA_DIRS","XDG_MENU_PREFIX","XDG_RUNTIME_DIR","XDG_SEAT","XDG_SESSION_DESKTOP","XDG_SESSION_ID","XDG_SESSION_TYPE","XDG_VTNR","XMODIFIERS"};
+		for(int i = 0 ; i < 60 ; i++)
+		{
+			if(getenv(exports[i]) <= 0)
+			{
+				printf("declare -x %s\n",exports[i]);
+				continue;
+			}
+			if(strcmp(getenv(exports[i]),"") == 0)
+			{
+				printf("declare -x %s\n",exports[i]);
+			}
+			else
+			{
+				printf("declare -x %s=\"%s\"\n",exports[i],getenv(exports[i]));
+			}
+		}
+	}
+	else
+	{
+		setenv(variable,temp_path,1);	
+	}
+	return;
+}
+void My_history(char *temp)
+{
+	char* token = strtok(temp," ");
+	int count = 0, num;
+	token = strtok(NULL," ");
+	while(token != NULL)
+	{
+		int val = atoi(token);
+		if(val == 0 && muti_zero(token, strlen(token) - 1) == 0)
+		{
+			if(count >= 1)
+			{
+				printf("history: too many arguments\n");
+			}
+			else
+			{
+				printf("history: %s: numeric argument required\n",token);	
+			}
+			return;
+		}
+		else if(val == 0 && muti_zero(token, strlen(token) - 1) == 1);
+		else if(val != 0)
+		{
+			count += 1;
+			if(count >= 2)
+			{
+				printf("history: too many arguments\n");
+				return;
+			}
+			num = val;
+		}
+		token = strtok(NULL," ");
+	}
+	if(count == 0)
+	{
+		print_history(1, num);
+	}
+	if(count == 1)
+	{
+		print_history(0, num);		
+	}
+	return;
 }
